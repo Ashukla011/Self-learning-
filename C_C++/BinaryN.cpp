a355cef8bace35218a2a72e4cca3142e115c5805
Ashukla011 <shuklavashu9696@gmail.com>
2025-06-16 00:00:38 +0530

    C_++_Code

--- C_C++/BinaryN.cpp
+++ C_C++/BinaryN.cpp
@@ -1,21 +1,6 @@
 #include <iostream>
 using namespace std;
 
-// Decimal to binary 
-int deciBinary (int a){
-    int ans = 0, pow = 1;
-    while (a>0)
-    {
-    int rem = a%2;
-     a/=2;
-     ans +=(rem*pow);
-     pow *= 10;
-    }
-    return ans;
-
-}
-
-// binary to decimal 
 
 
 int main (){
@@ -49,10 +34,9 @@ int main (){
 */
 
 // CODE 
- for ( int i =0;i<10;i++){
-    cout << deciBinary(i)<< endl;
- }
-    
+ 
+
+ 
  
 return 0;
 }
