6068a0c46dfe28ec4b914b6ac42b618e865ce811
Ashukla011 <shuklavashu9696@gmail.com>
2025-07-17 15:18:32 +0530

    nodejs

--- NodeJSCode/Day3/index.js
+++ NodeJSCode/Day3/index.js
@@ -1,6 +1,6 @@
 const http = require("http")
 const fs = require("fs")
-const server = http.createServer((req,res)=>{
+const server = http.createServer({encoding : "utf-8"},(req,res)=>{
     if(req.url == "/"){
         res.end("This is home page")
     }else if(req.url ==="/data"){
@@ -14,7 +14,19 @@ const server = http.createServer((req,res)=>{
        
        }
        })
-    }else{
+    }else if(req.url ='/addDetail' && req.method ==="POST",{encoding : "utf-8"}) {
+        let str= "";
+        req.on("data",(chunk)=>{
+          str = chunk;
+
+        })
+       req.on("end",()=>{
+        console.log(str)
+       })
+       console.log(str)
+        res.end("data has been entered")
+    }
+    else{
         console.log("Invailed!")
     }
 }) 
