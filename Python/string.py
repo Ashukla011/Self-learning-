b7c9770b87b3a645b1cf219abc2d54aee857340e
Ashukla011 <avnishkumar492862@gmail.com>
2025-12-24 11:08:27 +0530

    python

--- Python/string.py
+++ Python/string.py
@@ -1,7 +1,22 @@
 
 fruit = 'banana'
-letter = fruit[1] # print b
+animal = 'lion'
+fruit_animal = f"{fruit} and {animal}"
+print(f"combine both the string: {fruit_animal}")
 
+# add new line charecter \n 
+print("\nPython \nc++ \njava")
+
+# Stripping Whitespace : 
+# Extra whitespace can be confusing in your programs. To programmers, 
+# 'python' and 'python ' look pretty much the same. But to a program, they 
+# are two different strings
+
+lang = "python"
+print(lang)
+print(lang.rstrip())
+
+letter = fruit[1] # print 
 # Length 
 length = len(fruit)
 # last = fruit[length]
@@ -20,6 +35,6 @@ print(slicetwo) # Python
 
 # t strings are immutable
 greeting = 'Hello world'
-greeting[0] = 'J'
+# greeting[0] = 'J'
 # TypeError: 'str' object des not support item assignment
 # which means you can not change an existing string . 
