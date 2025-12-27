801004687081d188c8284fcbf4757bd689ab6760
Ashukla011 <avnishkumar492862@gmail.com>
2025-12-27 13:42:13 +0530

    python

---
+++ Python/variable.py
@@ -0,0 +1,22 @@
+# a value . for example
+
+age = 20
+name = "Pawan" 
+# age and name are variables here
+
+# Integers(int)
+# Floating-Pint Number (Float)
+# String (str) 
+# List (list) 
+# Dictionaries(dict)
+
+# other serveral data types of data that may be stored in variables
+
+temeratire = 97.6
+
+days_of_week = ["Monday", "Tuesday", "Sunday"]
+
+radius = float(input("Enter the radius"))
+area = 3.14 * radius**2
+
+print(f"The area of the circle is:", area)
