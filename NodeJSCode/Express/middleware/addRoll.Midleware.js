84cb4915c9e7a4fc9f69f71aa17e200d2936d2a5
Ashukla011 <avnishkumar492862@gmail.com>
2026-07-08 11:33:37 +0530

    node.js

--- NodeJSCode/Express/middleware/addRoll.Midleware.js
+++ NodeJSCode/Express/middleware/addRoll.Midleware.js
@@ -1,15 +1,12 @@
-const addRoll = (req,res,next)=>{
- 
-   
-        if(req.method = "POST"){
-         let roll = Math.random();
-        req.body.roll_no=roll ;
-        req.body.university = "CU"
-        next()
-        }
-    
-}
+const addRoll = (req, res, next) => {
+  if ((req.method = "POST")) {
+    let roll = Math.random();
+    req.body.roll_no = roll;
+    req.body.university = "CU";
+    next();
+  }
+};
 
-module.exports ={
-    addRoll
-}
+module.exports = {
+  addRoll,
+};
